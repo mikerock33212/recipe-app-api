1770d99301bb53a62652e3fc254ca16cd42dff9c
mikerock33212 <mikerock332@gmail.com>
2022-01-16 22:54:26 +0800

    fixed psycopg2 issue

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from core import models
+from . import models
 from django.utils.translation import gettext as _
 
 # Register your models here.
